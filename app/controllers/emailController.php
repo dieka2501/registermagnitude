4271ea3c7a371bb38941700486f76f7b4fc84413
dikdik <dieka.koes@gmail.com>
2016-05-09 11:35:54 +0700

    Apa aja

--- app/controllers/emailController.php
+++ app/controllers/emailController.php
@@ -26,11 +26,11 @@ class emailController Extends BaseController{
 	function not_coming(){
         $getce = $this->ce->get_not_checkin();
         foreach ($getce as $ces) {
-            $getvisitor         = $this->visitor->get_id($ces->id_pengunjung);
+            $getvisitor         = $this->visitor->getid($ces->id_pengunjung);
             if(filter_var($getvisitor->email,FILTER_VALIDATE_EMAIL)){
                 $detail['name']     = $getvisitor->nama_visitor;
                 echo $getvisitor->nama_visitor."<br>";
-                Mail::send('mail.thanks',$detail,function($m) use ($getvisitor){
+                Mail::send('mail.missyu',$detail,function($m) use ($getvisitor){
                     $m->from('no-reply@data-driven.asia')
                         ->to($getvisitor->email,$getvisitor->nama_visitor)
                         ->subject('Anda melewatkan hari ini, semoga anda dalam keadaan baik.');
