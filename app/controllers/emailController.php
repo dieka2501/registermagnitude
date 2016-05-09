f9e2f34ca080082fdf7c88447b3fe9eddcd63ef9
dikdik <dieka.koes@gmail.com>
2016-05-09 10:59:54 +0700

    Not Coming

--- app/controllers/emailController.php
+++ app/controllers/emailController.php
@@ -14,12 +14,29 @@ class emailController Extends BaseController{
                 $detail['name']     = $getvisitor->nama_visitor;
                 echo $getvisitor->nama_visitor."<br>";
                 Mail::send('mail.thanks',$detail,function($m) use ($getvisitor){
-                    $m->from('no-reply@indobuildtech')
-                        ->to($getvisitor->email,$getvisitor->nama_visitor)
-                        ->subject('Terima Kasih Atas Kedatangannya');
+                    $m->from('no-reply@data-driven.asia');
+                    $m->to($getvisitor->email,$getvisitor->nama_visitor);
+                    $m->subject('Terima Kasih Atas Kedatangannya');
                 });
             }
         }
 
 	}
+
+	function not_coming(){
+        $getce = $this->ce->get_not_checkin();
+        foreach ($getce as $ces) {
+            $getvisitor         = $this->visitor->get_id($ces->id_pengunjung);
+            if(filter_var($getvisitor->email,FILTER_VALIDATE_EMAIL)){
+                $detail['name']     = $getvisitor->nama_visitor;
+                echo $getvisitor->nama_visitor."<br>";
+                Mail::send('mail.thanks',$detail,function($m) use ($getvisitor){
+                    $m->from('no-reply@data-driven.asia')
+                        ->to($getvisitor->email,$getvisitor->nama_visitor)
+                        ->subject('Anda melewatkan hari ini, semoga anda dalam keadaan baik.');
+                });    
+            }
+               
+        }
+    }
 }
